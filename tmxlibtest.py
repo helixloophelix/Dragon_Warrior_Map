67cafa6e0d0617d239dda7be3e9651002e0ea17b
helixloophelix <tristane@Tristans-MacBook-Pro.local>
2016-09-19 02:28:01 -0700

    Debugging statements removed
    That feels better.

--- tmxlibtest.py
+++ tmxlibtest.py
@@ -10,13 +10,9 @@ tilefile = open("dragonwarriortiles.png")
 
 dwtileimage = tmxlib.image.open(tilefile.name)
 dwtiles = tmxlib.tileset.ImageTileset("dragonwarriortiles", (16, 16), dwtileimage, source=tilefile.name)
-# print(type(dwtiles))
-# print(len(dwtiles))
-# print(dwtiles.column_count)
-# print(type(dwtiles.tile_image(0)))
 
 #tmxlib has effectively no support for actually displaying tiles, so we're going to use OpenCV to cut them out
-#as numpy arrays
+#as numpy arrays; it might be cleaner if I used PIL instead.
 
 #Load the actual image using OpenCV
 tileset_pixels = cv2.imread(tilefile.name)
@@ -30,41 +26,26 @@ for index in range(0, len(dwtiles)):
     top_left_x, top_left_y = image_region.top_left
     bottom_right_x = top_left_x + image_region.width
     bottom_right_y = top_left_y + image_region.height
-    # print(top_left_x, top_left_y, bottom_right_x, bottom_right_y)
     test_pixels = tileset_pixels[top_left_x:bottom_right_x, top_left_y:bottom_right_y]
-    # print(test_pixels.size)
-    # print(test_pixels)
     #PIL is probably the right way to do this generally; I should be using the Image class more often
     color_test = Image.fromarray(test_pixels)
     colors = color_test.getcolors()
-    #tmxlib cheerfully loads tiles of transparent pixels as the empty set, so we need to get rid of those
+    #Fuschia tiles are only there to keep the tileset loader happy and functional, so we don't need them
+    #Also, tmxlib cheerfully loads tiles of transparent pixels as the empty set, so we need to get rid of those
     if test_pixels.size != 0 and (256, (255, 0, 255)) not in colors:
         tileset_number_to_tile_pixels[index] = test_pixels
     
 #Now we can load the world of Alefgard and begin classifying tiles
 
-# print len(tileset_number_to_tile_pixels.keys())
-# for key in sorted(tileset_number_to_tile_pixels):
-#     cv2.imshow("Tile", tileset_number_to_tile_pixels[key])
-#     cv2.waitKey(0)
-
 alefgard_map = cv2.imread("alefgard.png")
-# # print(alefgard_map.size)
-#
-# #Split the map into subarrays, first vertically and then horizontally
-#
+
+#Split the map into subarrays, first vertically and then horizontally
+
 alefgard_array_list = []
 horizontal_split = np.hsplit(alefgard_map, 128)
 for hsclice in horizontal_split:
     vertical_split = np.vsplit(hsclice, 128)
     alefgard_array_list = alefgard_array_list + vertical_split
-
-# print(len(alefgard_array_list))
-
-# for key in tileset_number_to_tile_pixels:
-#     print(tileset_number_to_tile_pixels[key])
-#     cv2.imshow("Tile", tileset_number_to_tile_pixels[key])
-#     cv2.waitKey(0)
     
 #Split the map into subarrays
 #I stole this off Stack Overflow and I'm not 100% sure why it works
@@ -84,9 +65,7 @@ for hsclice in horizontal_split:
 
 map_list = []
 for map_tile in alefgard_array_list:
-#     # print(map_tile)
-#     # cv2.imshow("Map Tile", map_tile)
-#     # cv2.waitKey(0)
+    
 #     #let's try replacing this with a list comprehension
 #
 #     # current_tile = [tile_number for tile_number in tileset_number_to_tile_pixels
@@ -105,15 +84,12 @@ for map_tile in alefgard_array_list:
 #     #     cv2.imshow("Tileset Tile", tileset_number_to_tile_pixels[tile_number])
 #     #     cv2.waitKey(0)
 #     #     map_list.append(tile_number)
-#
+
     for tile_number in tileset_number_to_tile_pixels:
         current_tile = tileset_number_to_tile_pixels[tile_number]
         tile_match_list = []
         if np.array_equal(map_tile, current_tile):
             tile_match_list.append(tile_number)
-            # print(type(tile_number))
-            # print("Tile number is %i") % tile_number
-            # print(tile_match_list)
             break
 
     if not tile_match_list:
@@ -124,22 +100,9 @@ for map_tile in alefgard_array_list:
         print("Error: Multiple tileset tiles match map tile")
     else:
         map_list.append(tile_number)
-#
+
 print(len(map_list))
 
-
-
-
-            # map_pointer_x += 1
-            # if map_pointer_x < 127:
-            #     map_tile_value = tile_number    #We have to do this because tile_number isn't really an integer
-            #     map_array([map_pointer_x, map_pointer_y]) = map_tile_value
-            # else:
-            #     #Wrap around when we reach the edge of the array
-            #     map_pointer_x = 0
-            #     map_pointer_y += 1
-            #     map_tile_value = tile_number
-            #     map_array([map_pointer_x, map_pointer_y]) = map_tile_value
 # oneD_map_array = np.fromiter(map_list, int)
 # map_array = np.reshape(oneD_map_array, (128,128))
 # # np.reshape(map_array, (128, 128, 3))
