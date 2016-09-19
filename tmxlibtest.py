a21914b681611580e5dea361bf9d038388a30e88
helixloophelix <tristane@Tristans-MacBook-Pro.local>
2016-09-19 02:22:58 -0700

    Script now recognizes all tiles in the map
    Tileset actually fixed this time, script heavily modified

--- tmxlibtest.py
+++ tmxlibtest.py
@@ -3,7 +3,7 @@
 import tmxlib
 import cv2
 import numpy as np
-from sklearn.feature_extraction import image
+from PIL import Image
 
 #Open up the tileset
 tilefile = open("dragonwarriortiles.png")
@@ -11,7 +11,8 @@ tilefile = open("dragonwarriortiles.png")
 dwtileimage = tmxlib.image.open(tilefile.name)
 dwtiles = tmxlib.tileset.ImageTileset("dragonwarriortiles", (16, 16), dwtileimage, source=tilefile.name)
 # print(type(dwtiles))
-print(len(dwtiles))
+# print(len(dwtiles))
+# print(dwtiles.column_count)
 # print(type(dwtiles.tile_image(0)))
 
 #tmxlib has effectively no support for actually displaying tiles, so we're going to use OpenCV to cut them out
@@ -22,9 +23,6 @@ tileset_pixels = cv2.imread(tilefile.name)
 
 #Slice up the image and store the slices in a dictionary relating them to tile numbers
 
-#tmxlib.image.open doesn't actually do anything with the color-key transparency, which is the fault of PIL,
-#as far as I can tell
-
 tileset_number_to_tile_pixels = {}
 for index in range(0, len(dwtiles)):
     #Use the ImageRegion information to get the coordinates for each slice
@@ -32,27 +30,34 @@ for index in range(0, len(dwtiles)):
     top_left_x, top_left_y = image_region.top_left
     bottom_right_x = top_left_x + image_region.width
     bottom_right_y = top_left_y + image_region.height
-    print(top_left_x, top_left_y, bottom_right_x, bottom_right_y)
+    # print(top_left_x, top_left_y, bottom_right_x, bottom_right_y)
     test_pixels = tileset_pixels[top_left_x:bottom_right_x, top_left_y:bottom_right_y]
-    if test_pixels.size != 0:   #tmxlib cheerfully loads transparent pixels as the empty set
+    # print(test_pixels.size)
+    # print(test_pixels)
+    #PIL is probably the right way to do this generally; I should be using the Image class more often
+    color_test = Image.fromarray(test_pixels)
+    colors = color_test.getcolors()
+    #tmxlib cheerfully loads tiles of transparent pixels as the empty set, so we need to get rid of those
+    if test_pixels.size != 0 and (256, (255, 0, 255)) not in colors:
         tileset_number_to_tile_pixels[index] = test_pixels
     
 #Now we can load the world of Alefgard and begin classifying tiles
 
-for key in sorted(tileset_number_to_tile_pixels):
-    cv2.imshow("Tile", tileset_number_to_tile_pixels[key])
-    cv2.waitKey(0)
+# print len(tileset_number_to_tile_pixels.keys())
+# for key in sorted(tileset_number_to_tile_pixels):
+#     cv2.imshow("Tile", tileset_number_to_tile_pixels[key])
+#     cv2.waitKey(0)
 
-# alefgard_map = cv2.imread("alefgard.png")
-# print(alefgard_map.size)
-
-#Split the map into subarrays, first vertically and then horizontally
-
-#alefgard_array_list = []
-# horizontal_split = np.hsplit(alefgard_map, 128)
-# for hsclice in horizontal_split:
-#     vertical_split = np.vsplit(hsclice, 128)
-#     alefgard_array_list = alefgard_array_list + vertical_split
+alefgard_map = cv2.imread("alefgard.png")
+# # print(alefgard_map.size)
+#
+# #Split the map into subarrays, first vertically and then horizontally
+#
+alefgard_array_list = []
+horizontal_split = np.hsplit(alefgard_map, 128)
+for hsclice in horizontal_split:
+    vertical_split = np.vsplit(hsclice, 128)
+    alefgard_array_list = alefgard_array_list + vertical_split
 
 # print(len(alefgard_array_list))
 
@@ -77,8 +82,8 @@ for key in sorted(tileset_number_to_tile_pixels):
 #The better way to do this is to use numpy's fromiter() method (np.fromiter() )
 #But this is best used with an actual function, so I'll try it when I clean this script up
 
-# map_list = []
-# for map_tile in alefgard_array_list:
+map_list = []
+for map_tile in alefgard_array_list:
 #     # print(map_tile)
 #     # cv2.imshow("Map Tile", map_tile)
 #     # cv2.waitKey(0)
@@ -101,24 +106,26 @@ for key in sorted(tileset_number_to_tile_pixels):
 #     #     cv2.waitKey(0)
 #     #     map_list.append(tile_number)
 #
-#     for tile_number in tileset_number_to_tile_pixels:
-#         current_tile = tileset_number_to_tile_pixels[tile_number]
-#         tile_match_list = []
-#         if np.array_equal(map_tile, current_tile):
-#             tile_match_list.append(tile_number)
-#             print(type(tile_number))
-#             print("Tile number is %i") % tile_number
-#             print(tile_match_list)
-#             break
-#
-#     if not tile_match_list:
-#         print("Error: tile not recognized")
-#     elif len(tile_match_list) >= 2:
-#         print("Error: Multiple tileset tiles match map tile")
-#     else:
-#         map_list.append(tile_number)
-            
+    for tile_number in tileset_number_to_tile_pixels:
+        current_tile = tileset_number_to_tile_pixels[tile_number]
+        tile_match_list = []
+        if np.array_equal(map_tile, current_tile):
+            tile_match_list.append(tile_number)
+            # print(type(tile_number))
+            # print("Tile number is %i") % tile_number
+            # print(tile_match_list)
+            break
 
+    if not tile_match_list:
+        print("Error: tile not recognized")
+        cv2.imshow("Unrecognized Tile", map_tile)
+        cv2.waitKey(0)
+    elif len(tile_match_list) >= 2:
+        print("Error: Multiple tileset tiles match map tile")
+    else:
+        map_list.append(tile_number)
+#
+print(len(map_list))
 
 
 
